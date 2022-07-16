0bedbccf0cc726f8a18d30700ee3bd91ae2afaa9
zaynlannister <zaynlannister@gmail.com>
2022-07-16 20:24:48 +0500

    add header adaptive

--- src/js/menu-burger.js
+++ src/js/menu-burger.js
@@ -1,9 +1,29 @@
 const menuToggle = document.querySelector('#menuToggle');
+const menu = document.querySelector('.header__nav-links');
+
+function showMenu(element) {
+    element.classList.toggle('active')
+}
+
+function closeMenu() {
+    menuToggle.type = 'button'
+    menu.classList.remove('active')
+    document.body.classList.remove('stop-scroll')
+}
 
 menuToggle.addEventListener('click', function() {
     if (this.type === "button") {
         this.type= "submit"
+        showMenu(menu)
+        document.body.classList.add('stop-scroll')
     } else {
         this.type= "button"
+        showMenu(menu)
+        document.body.classList.remove('stop-scroll')
     }
+})
+
+menu.addEventListener('click', event => {
+    console.log('sdd')
+    closeMenu()
 })
